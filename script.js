f747da4af91ad36e6f4ef6e2262aa5a43176d43e
Taras Mykolaichuk <mtarasio@gmail.com>
2023-03-30 11:05:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   $('.slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      variableWidth: true,
+      dots: true,
+   });
+})
+
+ 
+ 
+ 
+ 
+ 
+ 
